610e7b312dc6bfcb406a3d2f20c09cd30c240781
cheeptana <>
2026-08-27 12:14:10 +0700

    week07 commit

---
+++ app/week07/page.tsx
@@ -0,0 +1,15 @@
+import { Suspense } from "react";
+import { shops } from "./components/shopItem";
+import Loading from "./components/Loading";
+import ShopList from "./components/ShopList";
+
+export default function ShopPage(){
+    return (
+        <div className="max-w-3xl mx-auto mt-6">
+            <h1 className="text-3xl font-bold text-center">Shop List</h1>
+            <Suspense fallback={<Loading />}>
+                <ShopList data={shops} />
+            </Suspense>
+        </div>
+    );
+}
